df6d6a1b6fd2f1624c919c174f523ba1f98bfde7
BullAzumi <60474976+BullAzumi@users.noreply.github.com>
2020-02-02 05:40:31 +0100

    Update expedition.go

--- expedition.go
+++ expedition.go
@@ -3,9 +3,8 @@
  */
  
 /*
- * VERSION 1.13
+ * VERSION 1.2
  */
- 
 
 /* DESCRIPTION
  * Automatically sends expeditions when a slot or expedition slot is free.
@@ -18,17 +17,22 @@
 
 
 home = "M:4:363:4"                                      //set your expo home
-sendFrom = 353                                          //system lower limit
-sendTo = 373                                            //system upper limit
-rankOnePoints = 1342123587                              //how many points does the first place have
+sendFrom = 363                                          //system lower limit
+sendTo = 364                                            //system upper limit
+rankOnePoints = 1234567890                              //how many points does the first place have
 smallCargo = false                                      //would you fly with small cargos? true = yes / false = no
 usePathfinder = true                                    //Use pathfinder in automatically build
 HeavyFighter = true                                     //should add HeavyFighters? yes = true / no = false
-maxSlotsUse = 7                                         //how many slots should we use?
+maxSlotsUse = 1                                         //how many slots should we use?
 loop = false                                            //should we fly every round again?
 expoTime = 1                                            //set duration
 selfShips = false                                       //assemble the ships yourself or have them calculated automatically true = self / false = automatic
 useWave = true                                          //should we let everyone fly on a coordinate and only then switch? true = yes / false = no
+ignoreFleets = false                                    //ignores fleets are on move true = yes / false = no
+
+//!!!ONLY WORKS IF useWave IS TRUE!!!
+mineDebris = true                                       //should we mine debris fields? true = yes / false = no
+
 
 //Change if you are selfShipper!
 ship = {LIGHTFIGHTER : 0 ,
@@ -50,13 +54,172 @@ ship = {LIGHTFIGHTER : 0 ,
 //######################################## SETTINGS END ########################################
 
 
-settings = GetServer().Settings
-speed = settings.EconomySpeed
+speed = GetServer().Settings.EconomySpeed
 myGalaxy = 0
 SC = 0
 LC = 0
-tmpsend = 0
 ships = {}
+expoSlots = 0
+expoInUse = 0
+freeSlots = 0
+reserved = GetFleetSlotsReserved()
+fleets = {}
+send = true
+minSecs = 0
+debrisSys = 0
+downSys = 0
+upSys = 0
+
+
+//debris functions
+func sendMineDebris() {
+    LogDebug("Call function sendMineDebris()")
+    if mineDebris && send && useWave {
+        PathfinderNeeded = scanGala()
+        fleetsGet()
+        if PathfinderNeeded > 0 {
+            for {
+                if expoInUse < expoSlots && freeSlots > 0 {
+                    f, err = sendDebris(PathfinderNeeded)
+                    if err != nil {
+                        LogError(err)
+                    }else {
+                        LogInfo("Debris in work!")
+                    }
+                }else {
+                    fleetsGet()
+                }
+            }
+        }
+    }
+}
+
+func scanGala() {
+    LogDebug("Call function scanGala()")
+    systemInfo, err = GalaxyInfos(myGalaxy, debrisSys)
+    
+    if err != nil {
+        LogDebug(err)
+    }
+    if systemInfo.ExpeditionDebris.PathfindersNeeded > 0 {
+        LogDebug("Debris detected")
+        LogDebug("Need " + systemInfo.ExpeditionDebris.PathfindersNeeded + " Pathfinder")
+        return systemInfo.ExpeditionDebris.PathfindersNeeded
+    }else {
+        LogDebug("No Debris detected")
+        return systemInfo.ExpeditionDebris.PathfindersNeeded
+    }
+}
+
+func sendDebris(shipCounter) {
+    LogDebug("Call function sendDebris()")
+    
+    sTo = NewCoordinate(myGalaxy, debrisSys, 16, DEBRIS_TYPE)
+    fleet = NewFleet()
+    fleet.SetOrigin(home)
+    fleet.SetDestination(sTo)
+    fleet.SetMission(RECYCLEDEBRISFIELD)
+    fleet.AddShips(PATHFINDER, shipCounter)
+    return fleet.SendNow()
+}
+
+//expeditions function
+func doExpo() {
+    send = false
+    for expoInUse < maxSlotsUse{
+        for {
+            if expoInUse < expoSlots && freeSlots > 0 {
+                break;
+            }
+            fleetsGet()
+        }
+        if downSys <= upSys || downSys > upSys {
+            newFleet, err = sendExpo()
+            if err != nil {
+                LogError("Fleet reports errors: " + err)
+                send = false
+                continue
+            }else{
+                LogInfo("Fleet starts on the expedition! " + newFleet)
+                if minSecs < newFleet.BackIn {
+                    minSecs = newFleet.BackIn
+                }
+                fleetsGet()
+                send = true
+                if !useWave {
+                    debrisSys = downSys
+                    downSys = setSys()
+                }
+                Sleep(Random(3,7)*1000)
+            }
+        }
+    }
+}
+
+func sendExpo() {
+    LogDebug("Call function sendExpo()")
+    
+    sTo = NewCoordinate(myGalaxy, downSys, 16, PLANET_TYPE)        
+    fleet = NewFleet()
+    fleet.SetOrigin(home)
+    fleet.SetDestination(sTo)
+    LogInfo("Coords set to " + sTo )
+    fleet.SetSpeed(HUNDRED_PERCENT)
+    fleet.SetMission(EXPEDITION)
+    fleet.SetShips(*ships)
+    fleet.SetDuration(expoTime)
+    return fleet.SendNow()
+}
+
+func setMinSecs() {
+    LogDebug("Call function setMinSecs()")
+    for fleet in fleets {
+        if fleet.Mission == EXPEDITION {
+            if minSecs < fleet.BackIn {
+                minSecs = fleet.BackIn
+            }
+        }
+    }
+}
+
+func setSys() {
+    if sendFrom < sendTo {
+        return downSys++
+    }else {
+        return downSys--
+    }
+}
+
+func checkLoop() {
+    if downSys == upSys + 1 {
+            if loop {
+                LogInfo("What a feeling! My mens and womens and myself think a lot about these expeditions!")
+                LogInfo("But there is still so much unknown in these galaxies.")
+                LogInfo("We have to explore the sectors again! Time was running out! On another round!")
+                downSys = sendFrom
+            }else {
+                LogInfo("It was an honor to explore this unknown vastness of the galaxies!")
+                LogInfo("But our troops need to recover! The ships have to be brought up to scratch again.")
+                LogInfo("We thank the entire team for this excellent work and say goodbye for now!")
+                StopScript(__FILE__)
+            }
+        }
+}
+
+func customSleep(sleepTime) {
+    if sleepTime <= 0 {
+        sleepTime = Random(5, 10)
+    }
+    LogInfo("Wait " + ShortDur(sleepTime + 10))
+    Sleep((sleepTime + 10) * 1000)
+}
+
+func fleetsGet() {
+    fleets, slots = GetFleets()
+    expoSlots = slots.ExpTotal
+    expoInUse = slots.ExpInUse
+    freeSlots = slots.Total - slots.InUse - reserved
+}
 
 func errorHandler(){
     LogDebug("Call function errorHandler()")
@@ -182,107 +345,41 @@ func shipsSet() {
     }
 }
 
-func sendExpo() {
-    LogDebug("Call function sendExpo()")
-    
-    sTo = NewCoordinate(myGalaxy, sendFrom, 16, PLANET_TYPE)        
-    fleet = NewFleet()
-    fleet.SetOrigin(home)
-    fleet.SetDestination(sTo)
-    LogInfo("Coords set to " + sTo )
-    fleet.SetSpeed(HUNDRED_PERCENT)
-    fleet.SetMission(EXPEDITION)
-    fleet.SetShips(*ships)
-    fleet.SetDuration(expoTime)
-    return fleet.SendNow()
-}
-
 func doWork(){
     LogDebug("Call function doWork()")
+    errorHandler()                                                              //checking any value error
 
-    //Checking errors
-    errorHandler()
+    downSys = sendFrom
+    upSys = sendTo
 
-    //Check if fleets are still on the way when starting the script because of SaveFleet Script
-    waitTime = startExpo()
+    if !ignoreFleets {
+        customSleep(startExpo())                                                //checking fleets on going and wait until they are back 
+    }
 
-    LogInfo("Wait until all fleets are back" + ShortDur(waitTime + 10))
-    Sleep((waitTime + 10) * 1000)
-
-    //get Galaxy and set ships
-    GalaxyGet()
-    shipsSet()
+    GalaxyGet()                                                                 //get the Galaxy form home
+    shipsSet()                                                                  //set Ships automatically or manual
     
     for {
-        //get slots
-        fleets, slots = GetFleets()
-        reserved = GetFleetSlotsReserved()
-        //set time
-        minSecs = 0
-        for fleet in fleets {
-            if fleet.Mission == EXPEDITION {
-                if minSecs < fleet.BackIn {
-                    minSecs = fleet.BackIn
-                }
-            }
-        }
-        //calc free slots
-        expoSlots = slots.ExpTotal
-        expoInUse = slots.ExpInUse
-        freeSlots = slots.Total - slots.InUse - reserved
-        LogDebug("Expeditions available: " + expoSlots - expoInUse)
+        fleetsGet()                                                             //get slots
+
+        minSecs = setMinSecs()                                                  //set time
+        
+        fleetsGet()                                                             //calc free slots again
+        LogDebug("Expeditions available: " + (expoSlots - expoInUse))
         LogDebug("Slots available: " + freeSlots)
         
-        Sleep(4000)
-        //send fleet
-        for expoInUse < maxSlotsUse{
-            for {
-                if expoInUse < expoSlots && freeSlots > 0 {
-                    break;
-                }
-                expoSlots = slots.ExpTotal
-                expoInUse = slots.ExpInUse
-                freeSlots = slots.Total - slots.InUse - reserved
-            }
-            if sendFrom <= sendTo {
-                newFleet, err = sendExpo()
-                if err != nil {
-                    LogError("Fleet reports errors: " + err)
-                    contine
-                }else{
-                    LogInfo("Fleet starts on the expedition! " + newFleet)
-                    if minSecs < newFleet.BackIn {
-                        minSecs = newFleet.BackIn
-                    }
-                    expoInUse++
-                    if !useWave {
-                        tmpsend++
-                        sendFrom++ 
-                    }
-                    Sleep(Random(3,7)*1000)
-                }
-            }
+        doExpo()                                                                //send expo
+        
+        checkLoop()                                                             //checking is loop when he is finished
+        
+        if useWave && send {                                                    //wave tactics
+            debrisSys = downSys
+            downSys = setSys() 
         }
-            if sendFrom == sendTo + 1 {
-                if loop {
-                    LogInfo("What a feeling! My mens and womens and myself think a lot about these expeditions!")
-                    LogInfo("But there is still so much unknown in these galaxies.")
-                    LogInfo("We have to explore the sectors again! Time was running out! On another round!")
-                    sendFrom -= tmpsend
-                }else {
-                    LogInfo("It was an honor to explore this unknown vastness of the galaxies!")
-                    LogInfo("But our troops need to recover! The ships have to be brought up to scratch again.")
-                    LogInfo("We thank the entire team for this excellent work and say goodbye for now!")
-                    StopScript(__FILE__)
-                }
-            }        
-        if useWave {
-            tmpsend++
-            sendFrom++ 
-        } 
 
-        LogInfo("Checking again in " + ShortDur(minSecs + 10))
-        Sleep((minSecs + 10)*1000)
+        customSleep(minSecs)                                                    //set sleeper
+        
+        sendMineDebris()                                                        //after sleep checking debris
     }
 }
 doWork()
